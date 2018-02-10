feebeab6fa5fbe64be8855665f508d9199d1125f
CryptoJones <cryptojones@gmail.com>
2018-02-10 14:20:00 -0600

    Using Template per Michael's Recommendation

--- Program.cs
+++ Program.cs
@@ -14,17 +14,18 @@ namespace blogpost
             string linkTitle = String.Empty;
             string directory = "/var/www/www.cryptospace.com";
             string newPageName = now.ToString("u").ToString().Substring(0, 10).Replace("\\", "-") + ".html";
-            string firstHalf = "<!DOCTYPE html><html lang=\"en-US\"><head><meta charset=\"UTF-8\" /><meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\" /><!-- SEO --><title>Aaron K. Clark - CryptoJones</title><meta name=\"description\" content=\"Aaron K. Clark, CryptoJones, CV, Resume, Email, Cybersecurity, Software Engineering, Information Technology\" /><meta name=\"robots\" content=\"index, follow\" /><meta name=\"referrer\" content=\"always\" /><!-- Favicon --><link rel=\"icon\" type=\"image/png\" href=\"images/favicon.png\" sizes=\"32x32\"><!-- Styles --><link rel='stylesheet' href='css/split.css' type='text/css' media='screen' /><meta name=\"viewport\" content=\"width=device-width,initial-scale=1\" /></head><body id=\"fullsingle\" class=\"page-template-page-fullsingle-split\"><div class=\"fs-split\"><!-- Image Side --><div class=\"split-image\"></div><!-- Content Side --><div class=\"split-content\"><div class=\"split-content-vertically-center\"><div class=\"split-intro\"><span class=\"tagline\">" + newPageName.Substring(0, 10) + "</span></div><div class=\"split-bio\">";
-            string secondHalf = "</div><div class=\"split-lists\"><div class=\"split-list\"><ul><li><a href=\"index.html\">Go Back</a></li></div></div><div class=\"split-credit\"><p>&copy;2017 - Present <a href=\"#\">Aaron K. Clark</a></div></div></div></div></body></html>";
-
+            string firstHalf = string.Empty;
+            string incomingFileName = string.Empty;
+            string incomingData = string.Empty;
+            string secondHalf = String.Empty;
             //read blogpost.config.json
 
 
             try
             {
-                FileStream fileStream = new FileStream(directory + "/blogpost.config.json", FileMode.Open);
+                FileStream configFileStream = new FileStream("blogpost.config.json", FileMode.Open);
 
-                using (StreamReader reader = new StreamReader(fileStream))
+                using (StreamReader reader = new StreamReader(configFileStream))
                 {
                     documentTemp = reader.ReadToEnd();
                 }
@@ -35,7 +36,25 @@ namespace blogpost
 
                 // populate variables with object data
                 linkTitle = bp.title;
-                middle = bp.data;
+                incomingFileName = bp.data;
+
+                FileStream firstHalfFileStream = new FileStream("template_first_half", FileMode.Open);
+                using (StreamReader reader = new StreamReader(firstHalfFileStream))
+                {
+                    firstHalf = reader.ReadToEnd();
+                }
+
+                FileStream incomingDataFileStream = new FileStream(incomingFileName, FileMode.Open);
+                using (StreamReader reader = new StreamReader(incomingDataFileStream))
+                {
+                    incomingData = reader.ReadToEnd();
+                }
+
+                FileStream secondHalfFileStream = new FileStream(incomingFileName, FileMode.Open);
+                using (StreamReader reader = new StreamReader(secondHalfFileStream))
+                {
+                    secondHalf = reader.ReadToEnd();
+                }
 
             }
             catch (Exception ex)
